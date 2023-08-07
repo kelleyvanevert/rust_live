3e72788e62bc42a8b6591ec561a637e0a22bc1f1
Kelley van Evert <hello@klve.nl>
2023-08-07 22:48:08 +0200

    following a tut :P

---
+++ test_handmade_dsp/src/main.rs
@@ -0,0 +1,94 @@
+use rodio::{OutputStream, Source};
+use std::f32::consts::TAU;
+
+fn main() {
+    println!("Hello, world!");
+
+    let wave_table_size = 64;
+    let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
+
+    for n in 0..wave_table_size {
+        wave_table.push((TAU * n as f32 / wave_table_size as f32).sin());
+    }
+
+    let mut osc = WaveTableOsc::new(441_000, wave_table);
+
+    osc.set_frequency(440.0);
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    let _res = stream_handle.play_raw(osc.convert_samples());
+
+    std::thread::sleep(std::time::Duration::from_millis(100_000));
+}
+
+// 0.. S
+// 0.. tau
+// 1 .. 0 .. 1 [len 64]
+
+struct WaveTableOsc {
+    sample_rate: u32,
+    wave_table: Vec<f32>,
+    index: f32,
+    index_increment: f32,
+}
+
+impl WaveTableOsc {
+    fn new(sample_rate: u32, wave_table: Vec<f32>) -> Self {
+        Self {
+            sample_rate,
+            wave_table,
+            index: 0.0,
+            index_increment: 0.0,
+        }
+    }
+
+    fn set_frequency(&mut self, frequency: f32) {
+        self.index_increment = frequency * self.wave_table.len() as f32 / self.sample_rate as f32;
+    }
+
+    fn get_sample(&mut self) -> f32 {
+        let sample = self.lerp();
+        self.index += self.index_increment;
+        self.index %= self.wave_table.len() as f32;
+
+        sample
+    }
+
+    fn lerp(&self) -> f32 {
+        let index_trunc = self.index as usize;
+        let index_next = (index_trunc + 1) % self.wave_table.len();
+
+        let index_next_weight = self.index - (index_trunc as f32);
+        let index_trunc_weight = 1.0 - index_next_weight;
+
+        index_trunc_weight * self.wave_table[index_trunc]
+            + index_next_weight * self.wave_table[index_next]
+    }
+}
+
+impl Iterator for WaveTableOsc {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        return Some(self.get_sample());
+    }
+}
+
+impl Source for WaveTableOsc {
+    fn channels(&self) -> u16 {
+        1
+    }
+
+    fn sample_rate(&self) -> u32 {
+        self.sample_rate
+    }
+
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
+
+    fn total_duration(&self) -> Option<std::time::Duration> {
+        None
+    }
+}
